81d2d6d292b879471583c0fa017f30f3c71ed2db
arimasan <bearfaust4361@gmail.com>
2022-05-26 16:48:19 +0900

    wip 暫定ハンバーガーメニュー実装、showレスポンシブ対応

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,13 @@
 import Link from 'next/link'
 import styles from '../styles/Header.module.scss'
+import React, {useState} from "react"
 
 const Header = () => {
+    const [openMenu, setOpenMenu] = useState(false);
+    const menuFunction = () => {
+      setOpenMenu(!openMenu);
+    }
+
   return (
     <>
         <header>
@@ -27,19 +33,21 @@ const Header = () => {
             </div>
         </header>
         <div className={styles.hamburgerMenu} id="hamburger-menu">
-            <div className={styles.hamburgerMenuIcon} id="hamburger-menu-icon">
+            <div className={styles.hamburgerMenuIcon} id="hamburger-menu-icon" onClick={() => menuFunction()}>
                 <span></span>
                 <span></span>
                 <span></span>
             </div>
-            <div className={styles.hamburgerMenuList}>
-                <ul>
-                    <li><Link href="/#first-view"><a>直近の企画</a></Link></li>
-                    <li><Link href="/#event-archives"><a>過去アーカイブ</a></Link></li>
-                    <li><Link href="/#comunity-links"><a>コミュニティリンク集</a></Link></li>
-                    <li><Link href="/#about-fushigi-fes"><a>不思議RTAフェスとは？</a></Link></li>
-                    <li><Link href="/#special-thanks"><a>主催・運営</a></Link></li>
-                </ul>
+            <div className={openMenu ? styles.active: undefined}>
+                <div className={styles.hamburgerMenuList} >
+                    <ul>
+                        <li><Link href="/#first-view"><a>直近の企画</a></Link></li>
+                        <li><Link href="/#event-archives"><a>過去アーカイブ</a></Link></li>
+                        <li><Link href="/#comunity-links"><a>コミュニティリンク集</a></Link></li>
+                        <li><Link href="/#about-fushigi-fes"><a>不思議RTAフェスとは？</a></Link></li>
+                        <li><Link href="/#special-thanks"><a>主催・運営</a></Link></li>
+                    </ul>
+                </div>
             </div>
         </div>
     </>
