a205578db71f466f998f631d59e31e367b20b34a
suzuki28ab <suzuki28ab@yahoo.co.jp>
2022-06-02 01:55:52 +0900

    add edit url

--- src/pages/api/challenge-dungeon.ts
+++ src/pages/api/challenge-dungeon.ts
@@ -4,10 +4,16 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 const handleCreate = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method !== 'POST') res.json('error')
   const prisma = new PrismaClient()
-  const data = await prisma.challengeDungeon.create({ data: req.body }).catch(e => {
-    console.log(e)
-    return e
-  })
+  const data = await prisma.challengeDungeon
+    .upsert({
+      where: { EditUrl: req.body.EditUrl },
+      update: req.body,
+      create: req.body,
+    })
+    .catch(e => {
+      console.log(e)
+      return e
+    })
   res.json(data)
 }
 
