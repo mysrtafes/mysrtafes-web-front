8282e93362cc4a233699a99ea65dbc5e77f77cc9
suzuki28ab <suzuki28ab@yahoo.co.jp>
2022-06-11 18:34:51 +0900

    tweet埋め込みsp

--- src/components/page/index/OfficialLinks/OfficialLinks.tsx
+++ src/components/page/index/OfficialLinks/OfficialLinks.tsx
@@ -2,8 +2,10 @@ import DiscordLink from '@/components/DiscordLink/DiscordLink'
 import styles from '@/components/page/index/OfficialLinks/OfficialLinks.module.scss'
 import TweetList from '@/components/TweetList/TweetList'
 import TwitchLink from '@/components/TwitchLink/TwitchLink'
+import useBetterMediaQuery from '@/hooks/useBetterMediaQuery'
 
 const OfficialLinks = () => {
+  const isPC = useBetterMediaQuery('(min-width: 768px)')
   return (
     <div className={styles.officialLink} id="official-links">
       <div className={styles.inner}>
@@ -12,9 +14,11 @@ const OfficialLinks = () => {
             <TwitchLink />
             <DiscordLink />
           </div>
-          <div className={styles.officialLinkFlexContent}>
-            <TweetList />
-          </div>
+          {!isPC && (
+            <div className={styles.officialLinkFlexContent}>
+              <TweetList />
+            </div>
+          )}
         </div>
       </div>
     </div>
