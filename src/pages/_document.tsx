8b8534fb1acb902a317bc45fcb32ed0a774fb7e0
suzuki28ab <suzuki28ab@yahoo.co.jp>
2022-06-11 14:13:30 +0900

    add inter

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -22,6 +22,10 @@ const Document = () => {
             />
           </>
         )}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@500;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
