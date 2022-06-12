a5c125227a57ee5791c55693003a42ada00e495f
arimasan <bearfaust4361@gmail.com>
2022-06-12 20:31:51 +0900

    各種項目修正

---
+++ src/components/page/event/SearchBox/SelectBox/SelectBox.tsx
@@ -0,0 +1,13 @@
+import styles from '@/components/page/event/SearchBox/SelectBox/SelectBox.module.scss'
+
+const SelectBox = () => {
+  return (
+    <div className={styles.selectBox}>
+      <select>
+        <option>option1</option>
+      </select>
+    </div>
+  )
+}
+
+export default SelectBox
